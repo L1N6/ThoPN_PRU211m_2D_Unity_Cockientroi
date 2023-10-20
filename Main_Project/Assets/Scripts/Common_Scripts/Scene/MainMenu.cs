0fce6a2e52270314c1ba07412006e3600e2eab3d
L1N6 <119463897+L1N6@users.noreply.github.com>
2023-10-20 07:42:30 +0700

    code tro choi phan con ong 65%, add video intro

--- Main_Project/Assets/Scripts/Common_Scripts/Scene/MainMenu.cs
+++ Main_Project/Assets/Scripts/Common_Scripts/Scene/MainMenu.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Video;
 
 public class MainMenu : MonoBehaviour
 {
@@ -10,19 +11,25 @@ public class MainMenu : MonoBehaviour
 
     private void Start()
     {
-        SceneTransitionObject.SetActive(false);
+        StartCoroutine(LoadTransitionSceneStart(1.0f));
     }
     public void PlayGame()
     {
-        StartCoroutine(LoadTransitionScene());
+        StartCoroutine(LoadTransitionSceneEnd());
     }
 
-    private IEnumerator LoadTransitionScene()
+    private IEnumerator LoadTransitionSceneEnd()
     {
         SceneTransitionObject.SetActive(true);
         SceneTransition.Play("TransitionEnd");
         yield return new WaitForSeconds(1);
         SceneManager.LoadSceneAsync("Common_Scenes");
+    }
+
+    private IEnumerator LoadTransitionSceneStart(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        SceneTransitionObject.SetActive(false);
     }
 
     public void QuitGame()
