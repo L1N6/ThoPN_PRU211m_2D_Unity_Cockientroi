6cc965dd919dbf98501d1f4b8e507a2025c1f91e
HoangTHHE161351 <122241506+HoangTHHE161351@users.noreply.github.com>
2023-10-25 11:39:43 +0700

    update

--- Main_Project/Assets/Scripts/Fox_Scripts/EnemyAI.cs
+++ Main_Project/Assets/Scripts/Fox_Scripts/EnemyAI.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyAI : MonoBehaviour
+public class EnemyAIFox : MonoBehaviour
 {
     public Transform target;
 
