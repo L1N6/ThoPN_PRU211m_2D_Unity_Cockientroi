56faa0202c3d45b09087b0f9009a5d7093dbf11c
TranDuong1998 <113903907+TranDuong1998@users.noreply.github.com>
2023-11-06 21:52:59 +0700

    update

--- Main_Project/Assets/Scripts/Tiger _Script/CountTigerChild.cs
+++ Main_Project/Assets/Scripts/Tiger _Script/CountTigerChild.cs
@@ -10,7 +10,7 @@ public class CountTigerChild : MonoBehaviour
     int points = 0;
     GameObject Toad;
     Rigidbody2D ToadRigidbody2D;
-
+    private bool canProcessCollision = true;
     private void Start()
     {
         Toad = GameObject.FindGameObjectWithTag("Toad");
@@ -40,16 +40,7 @@ public class CountTigerChild : MonoBehaviour
         pointText.text = points.ToString();
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("Tiger Child") && !(ToadRigidbody2D.velocity.y < -0.5f))
-        {
-            points++;
-            UpdateHUD();
-        }
-    }
-
-    //private void OnTriggerEnter2D(Collider2D collision)
+    //private void OnCollisionEnter2D(Collision2D collision)
     //{
     //    if (collision.gameObject.CompareTag("Tiger Child") && !(ToadRigidbody2D.velocity.y < -0.5f))
     //    {
@@ -57,4 +48,28 @@ public class CountTigerChild : MonoBehaviour
     //        UpdateHUD();
     //    }
     //}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (canProcessCollision)
+        {
+            StartCoroutine(ProcessCollision(collision.gameObject));
+        }
+    }
+
+    private IEnumerator ProcessCollision(GameObject collidedObject)
+    {
+        canProcessCollision = false;
+
+        if (collidedObject.gameObject.CompareTag("Tiger Child") && !(ToadRigidbody2D.velocity.y < -0.5f))
+        {
+            Debug.Log("Collision or Trigger 2D");
+            points++;
+            UpdateHUD();
+        }
+
+        yield return new WaitForSeconds(0.2f);
+
+        canProcessCollision = true;
+    }
 }
